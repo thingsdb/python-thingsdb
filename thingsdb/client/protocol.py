b259a21c42c984bd2ac60dcfa3350944ac69dd25
Jeroen van der Heijden <jeroen@transceptor.technology>
2021-08-06 19:05:23 +0200

    Added access keywords, comments

--- thingsdb/client/protocol.py
+++ thingsdb/client/protocol.py
@@ -29,6 +29,15 @@ from ..exceptions import WriteUVError
 from ..exceptions import ZeroDivisionError
 
 
+class Access(enum.IntEnum):
+    QUERY = 0x01
+    CHANGE = 0x02
+    GRANT = 0x04
+    JOIN = 0x08
+    RUN = 0x10
+    FULL = 0x1f
+
+
 class Proto(enum.IntEnum):
     # Events
     ON_NODE_STATUS = 0x00
