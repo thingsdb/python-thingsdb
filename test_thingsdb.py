e974fae1b959ef28ff5f9931a202ba0bc12f30ec
Jeroen van der Heijden <jeroen@cesbit.com>
2021-09-14 11:39:20 +0200

    Added test travis

---
+++ test_thingsdb.py
@@ -0,0 +1,28 @@
+import unittest
+import asyncio
+from thingsdb.client import Client
+
+
+class TestPlayground(unittest.TestCase):
+
+    async def async_test_playground(self):
+        want = "Welcome at ThingsDB!"
+
+        client = Client(ssl=True)
+
+        await client.connect('playground.thingsdb.net', 9400)
+        try:
+            await client.authenticate('Fai6NmH7QYxA6WLYPdtgcy')
+            data = await client.query(
+                code='.greetings[index];',
+                index=1,
+                scope='//Doc')
+            self.assertEqual(data, want)
+
+        finally:
+            client.close()
+            await client.wait_closed()
+
+    def test_playground(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.async_test_playground())
